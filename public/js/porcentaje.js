4dddbf4b3b287959368cab214e2e953b8f60f571
Brayan-g15 <b.sleider15@gmail.com>
2023-12-18 18:34:05 -0500

    intento 1

---
+++ public/js/porcentaje.js
@@ -0,0 +1,26 @@
+var saldo= 10;// variable saldo deposito DB
+let ganancias = 0; // variable saldo ganancias DB
+function calcularPorcentaje(){
+//     if(saldo<=0){
+//         console.log('saldo insuficiente')
+//     } else {
+
+//         let ejecutar = setInterval(imprimir, 3000)
+
+//         function porcentaje(numero){
+//             return numero / 100 * 3;
+            
+//         }
+//         let total = porcentaje(saldo)
+//         console.log(total)
+//         function imprimir(){
+//             let gananciaTotal = ganancias += total
+//             console.log(gananciaTotal)
+//         }  
+//     }
+}
+
+
+export default calcularPorcentaje;
+
+
