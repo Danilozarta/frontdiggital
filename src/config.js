fad88b99a2cb659595ba56c71ea6e2b5f5211f46
Brayan-g15 <b.sleider15@gmail.com>
2023-12-18 21:01:49 -0500

    modifique variables para conexion a DB de railway

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const PORT = process.env.PORT || 3000
 
-export const DB_HOST = process.env.DB_HOST || 'containers-us-west-82.railway.app'
+export const DB_HOST = process.env.DB_HOST || 'monorail.proxy.rlwy.net'
 export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'PD5MlsiTldH6t85BH7ot'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'ecb-B6a6BHadB22GG6G32b153H316HH4'
 export const DB_NAME = process.env.DB_NAME || 'railway'
-export const DB_PORT = process.env.DB_PORT || 7719
+export const DB_PORT = process.env.DB_PORT || 18757
